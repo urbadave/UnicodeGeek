5bbb20f89a3c08ca9492008071d347a692041d97
David Urbanek <durbanek@comcast.net>
2023-01-31 21:35:39 -0700

    Got all tools to build FixerMaster

---
+++ UnicodeGeek/UnicodeFixer/FixerMaster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeFixer;
+
+public class FixerMaster
+{
+    public Dictionary<int, LetterFixer> FixerList { get; set; } = new Dictionary<int, LetterFixer>();
+}
---
+++ UnicodeGeek/UnicodeFixer/LetterFixer.cs
@@ -0,0 +1,19 @@
+﻿namespace UnicodeFixer;
+
+public class LetterFixer
+{
+    public int Code { get; set; }
+    public bool Drop { get; set; }
+    public bool Keep { get; set; }
+    public string? Replace { get; set; }
+
+    public LetterFixer() { }
+
+    public LetterFixer(int code, bool drop, bool keep, string? replace)
+    {
+        Code = code;
+        Drop = drop;
+        Keep = keep;
+        Replace = replace;
+    }
+}
